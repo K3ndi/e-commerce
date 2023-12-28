d86acb5bf408714790a5fd602f124c139b0c56b6
Kendi Gjepali <okendigjepali99@gmail.com>
2023-12-28 15:24:24 +0100

    set up sass

---
+++ client/src/components/index.tsx
@@ -0,0 +1,5 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+//index.tsx components
+export { Footer, Header };
--- client/src/pages/layout/Layout.tsx
+++ client/src/pages/layout/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { Footer, Header } from "../../components";
 
 export default function Layout() {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
