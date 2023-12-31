e799248c0e752358eb8bd62ad349ff5fb1756216
Kendi Gjepali <okendigjepali99@gmail.com>
2023-12-31 02:44:18 +0100

    set up database connection

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,22 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
+const AppError = require('./utils/appError');
 
 const app = express();
 
 // 1. MIDDLEWARES
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-//you can access it locally like this: 127.0.0.1:3000/overview.html
-app.use(express.static(`${__dirname}/public`));
+//error handler to catch the bad url requests
+//when a user hits an url that doesnt exist
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  //it will always assume that what we pass to the next its gona be an error
+  //and if there are other middlewares it will skip them, and jump us to the error handling middleware
+});
 
 module.exports = app;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,26 @@
-const mongoose = require("mongoose");
-
 //environement variable needs to be before the application
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
-const app = require("./app");
+const { Sequelize } = require('sequelize');
+
+const app = require('./app');
+
+const sequelize = new Sequelize('sql_store', 'root', '1234', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3306',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully MYSQL DATABASE.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+  });
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
