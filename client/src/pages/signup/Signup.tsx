6e22ed3921ab4ef97c383cab63fee1515a229933
AmriNuzi <68481761+AmriNuzi@users.noreply.github.com>
2024-01-03 16:31:11 +0100

    fixed

--- client/src/pages/signup/Signup.tsx
+++ client/src/pages/signup/Signup.tsx
@@ -1,6 +1,9 @@
 import image1 from "./pic1.jpg";
 import "../../styles/signup/signup.scss";
 import { useState } from "react";
+import Login from "../login/Login";
+import { Link } from "react-router-dom";
+
 
 function Signup() {
   const [name, setName] = useState("");
@@ -14,27 +17,26 @@ function Signup() {
       </div>
       <div className="right-column">
         <div className="signup-form">
-        <h2>Create an account</h2>
-        <p>Enter your details below </p>
-          {/* <br /> */}
+          <h2>Create an account</h2>
+          <p>Enter your details below </p>
         <div className="form">
           <div className="input-box" >
-            <input type="text" placeholder="Name" style={{width: '300px', height:'50px'}} value={name} />
+            <input type="text" placeholder="Name"  value={name} />
           </div>
-         {/* <br /> */} 
           <div className="input-box">
-            <input type="email" placeholder="Email" style={{width: '300px', height:'50px'}} value={email}/>
+            <input type="email" placeholder="Email" value={email}/>
           </div>
-
           <div className="input-box">
-          {/* <br /> */}
-            <input type="password" placeholder="Password" style={{width: '300px', height:'50px'}} value={password}/>
+            <input type="password" placeholder="Password" value={password}/>
           </div>
-          <div className="input-box-button">
-          {/* <br />  */}
-            <button type="submit" style={{width: '300px', height:'50px', color: 'white', borderRadius:"6px", backgroundColor:'rgba(241, 77, 77)'}}>Create Account</button> 
-          </div>
+          <div className="input-box">
+            <button type="submit" >Create Account</button> 
+          </div>          
         </div>
+        <p>Already have account? 
+          <Link to="/login" > Login</Link>
+        </p>
+
       </div>    
       </div>
     </div>
