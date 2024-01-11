d0a0dbe7a8e684b6e1ff23e30b58822d5ee63e0d
AmriNuzi <68481761+AmriNuzi@users.noreply.github.com>
2024-01-11 21:33:24 +0100

    login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,5 +1,39 @@
+import image1 from "./pic1.jpg";
+import "../../styles/login/login.scss";
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+
+
 function Login() {
-  return <div>Login page</div>;
+  // const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return( 
+    <div className="login">
+      <div className="imagelogin">
+        <img src={image1} alt="" />
+      </div>
+      <div className="right-column">
+        <div className="login-form">
+          <h2>Log in to Exclucive</h2>
+          <p>Enter your details below</p>
+        <div className="form">
+          <div className="input-box">
+            <input type="email" placeholder="Email" value={email}/>
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" value={password}/>
+          </div>
+          <div className="input-box">
+            <button type="submit" >Log In</button> 
+          </div>          
+        </div>
+      </div>    
+      </div>
+    </div>
+
+  )
 }
 
 export default Login;
