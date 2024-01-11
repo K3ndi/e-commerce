bb7cfdc811b4c76f52c91a016da7c4d2f2bc8a62
kendi <okendigjepali99@gmail.com>
2024-01-11 17:22:24 +0100

    /

--- api/src/server.ts
+++ api/src/server.ts
@@ -22,7 +22,7 @@ sequelize
     console.error('Unable to connect to the database: ', error);
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8888;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
