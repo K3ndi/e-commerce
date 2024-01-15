df97b57f900a6c1ad4a9fe9cf7f2d0cd0f53b362
Kendi Gjepali <okendigjepali99@gmail.com>
2024-01-15 21:06:44 +0100

    .

--- client/src/pages/signup/Signup.tsx
+++ client/src/pages/signup/Signup.tsx
@@ -7,39 +7,64 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  return( 
+  return (
     <div className="sign-up">
       <div className="imagesignup">
         <img src={image1} alt="" />
       </div>
       <div className="right-column">
         <div className="signup-form">
-        <h2>Create an account</h2>
-        <p>Enter your details below </p>
+          <h2>Create an account</h2>
+          <p>Enter your details below </p>
           {/* <br /> */}
-        <div className="form">
-          <div className="input-box" >
-            <input type="text" placeholder="Name" style={{width: '300px', height:'50px'}} value={name} />
-          </div>
-         {/* <br /> */} 
-          <div className="input-box">
-            <input type="email" placeholder="Email" style={{width: '300px', height:'50px'}} value={email}/>
-          </div>
+          <div className="form">
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Name"
+                style={{ width: "300px", height: "50px" }}
+                value={name}
+              />
+            </div>
+            {/* <br /> */}
+            <div className="input-box">
+              <input
+                type="email"
+                placeholder="Email"
+                style={{ width: "300px", height: "50px" }}
+                value={email}
+              />
+            </div>
 
-          <div className="input-box">
-          {/* <br /> */}
-            <input type="password" placeholder="Password" style={{width: '300px', height:'50px'}} value={password}/>
-          </div>
-          <div className="input-box-button">
-          {/* <br />  */}
-            <button type="submit" style={{width: '300px', height:'50px', color: 'white', borderRadius:"6px", backgroundColor:'rgba(241, 77, 77)'}}>Create Account</button> 
+            <div className="input-box">
+              {/* <br /> */}
+              <input
+                type="password"
+                placeholder="Password"
+                style={{ width: "300px", height: "50px" }}
+                value={password}
+              />
+            </div>
+            <div className="input-box-button">
+              {/* <br />  */}
+              <button
+                type="submit"
+                style={{
+                  width: "300px",
+                  height: "50px",
+                  color: "white",
+                  borderRadius: "6px",
+                  backgroundColor: "rgba(241, 77, 77)",
+                }}
+              >
+                Create Account
+              </button>
+            </div>
           </div>
         </div>
-      </div>    
       </div>
     </div>
-
-  )
+  );
 }
 
 export default Signup;
