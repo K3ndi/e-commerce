4dbe3bd598f2279dbe896b5b080a82d6b858c6c4
Kendi Gjepali <okendigjepali99@gmail.com>
2024-02-28 23:32:47 +0100

    Working on front end

--- client/src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-
-//index.tsx components
-export { Footer, Header };
--- client/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import Home from "./home/Home";
-import Layout from "./layout/Layout";
-import Login from "./login/Login";
-import Signup from "./signup/Signup";
-
-export { Signup, Layout, Login, Home };
